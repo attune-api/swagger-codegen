6b5a6f0c202546e95dbeac23d577ec595399c254
Jason Gritman <jason@attune.co>
2014-07-07 17:22:12 -0700

    documentation and code review feedback

--- src/main/scala/AttuneRubyGenerator.scala
+++ src/main/scala/AttuneRubyGenerator.scala
@@ -1,5 +1,7 @@
 import com.wordnik.swagger.codegen.BasicRubyGenerator
+import com.wordnik.swagger.model._
 
+import java.beans.Introspector
 import java.io.File
 
 object AttuneRubyGenerator extends BasicRubyGenerator {
@@ -19,7 +21,69 @@ object AttuneRubyGenerator extends BasicRubyGenerator {
     fixedName(0).toUpper + fixedName.substring(1)
   }
 
-  override def toApiFilename(name: String) = name.toLowerCase.replaceAll("(\\{|\\})","")
+  override def toApiFilename(name: String) = toUnderscore(name).toLowerCase.replaceAll("(\\{|\\})","")
+  override def toModelFilename(name: String) = toUnderscore(name).toLowerCase.replaceAll("(\\{|\\})","")
+
+  override def toUnderscore(name: String): String = {
+    "[A-Z\\d]".r.replaceAllIn(Introspector.decapitalize(name), {m => "_" + m.group(0).toLowerCase() })
+  }
+
+  override def toDeclaration(obj: ModelProperty) = {
+    var dataType = obj.`type`(0).toUpper + obj.`type`.substring(1)
+
+    dataType match {
+      case "Set" => dataType = "Array"
+      case "Int" => dataType = "Integer"
+      case "Date-time" => dataType = "String"
+      case e: String => e
+    }
+
+    val defaultValue = toDefaultValue(dataType, obj)
+    dataType match {
+      case "Array" => {
+        val inner = {
+          obj.items match {
+            case Some(items) => {
+              if(items.ref != null)
+                items.ref.getOrElse(items.`type`)
+              else
+                items.`type`
+            }
+            case _ => {
+              println("failed on " + dataType + ", " + obj)
+              throw new Exception("no inner type defined")
+            }
+          }
+        }
+        dataType = "Array<" + toDeclaredType(inner) + ">"
+      }
+      case _ =>
+    }
+    (dataType, defaultValue)
+  }
+
+  override def toDeclaredType(dt: String): String = {
+    typeMapping.getOrElse(dt, "Attune::Model::" + dt)
+  }
+
+  override def typeMapping = Map(
+    "float" -> "BigDecimal",
+    "long" -> "Integer",
+    "double" -> "BigDecimal",
+    "integer" -> "Integer",
+    "Array" -> "Array",
+    "boolean" -> "Boolean",
+    "string" -> "String",
+    "date" -> "String",
+    "dateTime" -> "String"
+  )
+
+  override def languageSpecificPrimitives = Set(
+    "String",
+    "BigDecimal",
+    "Integer",
+    "Array",
+    "Boolean")
 
   override def supportingFiles = List(
     ("attune.mustache", destinationDir, "attune.rb"),
